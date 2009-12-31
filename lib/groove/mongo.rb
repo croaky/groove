cba76947986727eae4c1d05ed8fea1d690f6de05
Dan Croak <dcroak@thoughtbot.com>
2009-12-31 15:42:13 -0500

    cannot depend on ENV variable for mongo db name

--- lib/groove/mongo.rb
+++ lib/groove/mongo.rb
@@ -2,7 +2,7 @@ require 'mongo'
 
 include Mongo
 
-DB = Connection.new(ENV['DATABASE_URL'] || 'localhost').db(ENV['DATABASE'])
+DB = Connection.new(ENV['DATABASE_URL'] || 'localhost').db(ENV['DATABASE'] || 'app')
 if ENV['DATABASE_USER'] && ENV['DATABASE_PASSWORD']
   DB.authenticate(ENV['DATABASE_USER'], ENV['DATABASE_PASSWORD'])
 end
